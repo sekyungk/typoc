931120a24c4355656fb10d151ccf4bb6c174a910
sekyungk <sekyungk@outlook.com>
2021-07-01 15:43:12 +0900

    Add format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,26 +7,52 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: "Roboto";
     font-style: normal;
-    src: url("/fonts/Roboto-Regular.ttf") format("truetype");
+    font-weight: 400;
+    src: url('/fonts/roboto-v27-latin-regular.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/roboto-v27-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/roboto-v27-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/roboto-v27-latin-regular.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/roboto-v27-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/roboto-v27-latin-regular.svg#Roboto') format('svg'); /* Legacy iOS */
   }
   @font-face {
     font-family: "Roboto";
     font-style: normal;
-    src: url("/fonts/Roboto-Bold.ttf") format("truetype");
-    font-weight: bold;
+    font-weight: 700;
+    src: url('/fonts/roboto-v27-latin-700.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/roboto-v27-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/roboto-v27-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/roboto-v27-latin-700.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/roboto-v27-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/roboto-v27-latin-700.svg#Roboto') format('svg'); /* Legacy iOS */
   }
   @font-face {
     font-family: "Noto Sans KR";
     font-style: normal;
-    src: url("/fonts/NotoSansKR-Regular.otf") format("opentype");
     unicode-range: U+AC00-D7A3;
+    font-weight: 400;
+    src: url('/fonts/noto-sans-kr-v13-korean-regular.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/noto-sans-kr-v13-korean-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/noto-sans-kr-v13-korean-regular.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/noto-sans-kr-v13-korean-regular.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/noto-sans-kr-v13-korean-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/noto-sans-kr-v13-korean-regular.svg#NotoSansKR') format('svg'); /* Legacy iOS */
   }
   @font-face {
     font-family: "Noto Sans KR";
     font-style: normal;
-    src: url("/fonts/NotoSansKR-Bold.otf") format("opentype");
     unicode-range: U+AC00-D7A3;
-    font-weight: bold;
+    font-weight: 700;
+    src: url('/fonts/noto-sans-kr-v13-korean-700.eot'); /* IE9 Compat Modes */
+    src: local(''),
+         url('/fonts/noto-sans-kr-v13-korean-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+         url('/fonts/noto-sans-kr-v13-korean-700.woff2') format('woff2'), /* Super Modern Browsers */
+         url('/fonts/noto-sans-kr-v13-korean-700.woff') format('woff'), /* Modern Browsers */
+         url('/fonts/noto-sans-kr-v13-korean-700.ttf') format('truetype'), /* Safari, Android, iOS */
+         url('/fonts/noto-sans-kr-v13-korean-700.svg#NotoSansKR') format('svg'); /* Legacy iOS */
   }
 
   body {
